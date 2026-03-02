f3ba9907af00816dfa07f749cf8c8d01d66a0e76
savan rathod <savan.rathod@outlook.com>
2026-03-02 13:39:34 +0530

    remove unused coded

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -73,7 +73,7 @@ export function Form() {
           errors={state.errors}
         />
         <ReCAPTCHA
-          sitekey="6Lfj9NYfAAAAAP8wPLtzrsSZeACIcGgwuEIRvbSg"
+          sitekey="6Lck9HwsAAAAAHzqkodUZWimSqV8InapV74dV6G1"
           onChange={(e) => {
             setIsHuman(true)
           }}
